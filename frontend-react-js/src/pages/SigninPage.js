2a3f9a4036c8491dc8057b6d0c31286aaf640257
jhargett1 <joshhargett.jh@gmail.com>
2023-03-04 19:07:22 +0000

    committing end of stream changes

--- frontend-react-js/src/pages/SigninPage.js
+++ frontend-react-js/src/pages/SigninPage.js
@@ -4,7 +4,7 @@ import {ReactComponent as Logo} from '../components/svg/logo.svg';
 import { Link } from "react-router-dom";
 
 // [TODO] Authenication
-import Cookies from 'js-cookie'
+import { Auth } from 'aws-amplify';
 
 export default function SigninPage() {
 
@@ -13,17 +13,22 @@ export default function SigninPage() {
   const [errors, setErrors] = React.useState('');
 
   const onsubmit = async (event) => {
-    event.preventDefault();
     setErrors('')
-    console.log('onsubmit')
-    if (Cookies.get('user.email') === email && Cookies.get('user.password') === password){
-      Cookies.set('user.logged_in', true)
-      window.location.href = "/"
-    } else {
-      setErrors("Email and password is incorrect or account doesn't exist")
-    }
-    return false
+    event.preventDefault();
+      Auth.signIn(email, password)
+        .then(user => {
+          localStorage.setItem("access_token", user.signInUserSession.accessToken.jwtToken)
+          window.location.href = "/"
+        })
+        .catch(error => { 
+          if (error.code == 'UserNotConfirmedException') {
+            window.location.href = "/confirm"
+          }
+          setErrors(error.message) 
+        });
+        return false
   }
+  
 
   const email_onchange = (event) => {
     setEmail(event.target.value);
