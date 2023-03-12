ab3ba33917bf8c430a14e392766625b5e396326a
jhargett1 <joshhargett.jh@gmail.com>
2023-03-12 16:47:59 +0000

    bash commands for psql

--- backend-flask/db/schema.sql
+++ backend-flask/db/schema.sql
@@ -7,6 +7,7 @@ CREATE TABLE public.users (
   uuid UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
   display_name text,
   handle text,
+  email text,
   cognito_user_id text,
   created_at TIMESTAMP default current_timestamp NOT NULL
 );
