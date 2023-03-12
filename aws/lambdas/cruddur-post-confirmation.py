de230ca8b330b511e1b07128a236cb176e6a7748
jhargett1 <joshhargett.jh@gmail.com>
2023-03-12 21:53:07 +0000

    post confirmation lambda

--- aws/lambdas/cruddur-post-confirmation.py
+++ aws/lambdas/cruddur-post-confirmation.py
@@ -1,5 +1,6 @@
 import json
 import psycopg2
+import os
 
 def lambda_handler(event, context):
     user = event['request']['userAttributes']
@@ -11,23 +12,26 @@ def lambda_handler(event, context):
     user_handle       = user['preferred_username']
     user_cognito_id   = user['sub']
     try:
-        conn = psycopg2.connect(os.getenv('CONNECTION_URL'))
-        cur = conn.cursor()
+        print('entered-try')
 
         sql = f"""
-            "INSERT INTO users (
+            INSERT INTO public.users (
                 display_name,
                 email, 
                 handle, 
                 cognito_user_id
                 ) 
-            VALUES(
-                {user_display_name}, 
-                {user_email},
-                {user_handle}, 
-                {user_cognito_id}
-            )"
+             VALUES(
+                '{user_display_name}', 
+                '{user_email}',
+                '{user_handle}', 
+                '{user_cognito_id}'
+            )
         """
+        print('SQL Statement -----')
+        print(sql)
+        conn = psycopg2.connect(os.getenv('CONNECTION_URL'))
+        cur = conn.cursor()        
         cur.execute(sql)
         conn.commit() 
 
@@ -38,5 +42,4 @@ def lambda_handler(event, context):
             cur.close()
             conn.close()
             print('Database connection closed.')
-
     return event
