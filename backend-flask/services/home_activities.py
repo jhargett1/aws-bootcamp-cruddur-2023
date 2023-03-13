ea4b6c857996219518b01bbb35163faf9cc322f9
jhargett1 <joshhargett.jh@gmail.com>
2023-03-13 22:47:57 +0000

    refactor the db library

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,21 +1,19 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
 
-from lib.db import pool, query_wrap_array
+from lib.db import db
 
 #tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
-  def run(cognito_user_id=None):
-    print("=====home-activities")    
+  def run(cognito_user_id=None):    
    #logger.info("HomeActivities")
     #with tracer.start_as_current_span("home-activites-mock-data"):
       #span = trace.get_current_span()
       #now = datetime.now(timezone.utc).astimezone()      
       #span.set_attribute("app.now", now.isoformat())
-
-    sql = query_wrap_array("""
-    SELECT
+    results = db.query_array_json("""
+      SELECT
         activities.uuid,
         users.display_name,
         users.handle,
@@ -30,16 +28,4 @@ class HomeActivities:
       LEFT JOIN public.users ON users.uuid = activities.user_uuid
       ORDER BY activities.created_at DESC
     """)
-    print("SQL=========")    
-    print(sql)
-    print("SQL+++++++++")       
-    with pool.connection() as conn:
-      with conn.cursor() as cur:
-        cur.execute(sql)
-        # this will return a tuple
-        # the first field being the data
-        json = cur.fetchone()
-    print("---------") 
-    print(json[0])      
-    return json[0]   
     return results
