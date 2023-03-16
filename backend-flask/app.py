168a24765edfeabf473beaab89f83c3e59d2c97a
jhargett1 <joshhargett.jh@gmail.com>
2023-03-16 17:27:00 +0000

    working through postgres issues

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -206,7 +206,7 @@ def data_search():
 @app.route("/api/activities", methods=['POST','OPTIONS'])
 @cross_origin()
 def data_activities():
-  user_handle  = 'joshhargett'
+  user_handle  = 'andrewbrown'
   message = request.json['message']
   ttl = request.json['ttl']
   model = CreateActivity.run(message, user_handle, ttl)
