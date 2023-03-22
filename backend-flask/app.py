229753ce7c8bb2e5ef18bddbf90f0b5c6185ef27
jhargett1 <joshhargett.jh@gmail.com>
2023-03-22 22:39:26 +0000

    further into implement conver. updated seed.sql again to fix errors

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import request
 from flask_cors import CORS, cross_origin
 import os 
-import sys
+import sys 
 
 from services.home_activities import *
 from services.notifications_activities import *
--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,13 +1,12 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown','andrewbrown@exampro.co' , 'andrewbrown' ,'401a3fa1-0bd7-4d77-bf4c-d8fde2521edb'),
-  ('Andrew Bayko','bayko@exampro.co' , 'bayko' ,'MOCK');
+  ('Josh Hargett','joshhargett.jh@gmail.com' , 'joshhargett' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'joshhargett' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
