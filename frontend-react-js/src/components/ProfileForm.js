ac8bdb86a2b1537ba1a65329fce7819dc1830810
jhargett1 <joshhargett.jh@gmail.com>
2023-06-10 14:12:22 +0000

    Week X Sync Tool for static website hosting - Setup static building for our application

--- frontend-react-js/src/components/ProfileForm.js
+++ frontend-react-js/src/components/ProfileForm.js
@@ -48,7 +48,7 @@ export default function ProfileForm(props) {
     const filename = file.name
     const size = file.size
     const type = file.type
-    const preview_image_url = URL.createObjectURL(file)
+    // const preview_image_url = URL.createObjectURL(file)
     console.log(filename,size,type)
     const fileparts = filename.split('.')
     const extension = fileparts[fileparts.length-1]
@@ -89,7 +89,7 @@ export default function ProfileForm(props) {
           display_name: displayName
         }),
       });
-      let data = await res.json();
+      // let data = await res.json();
       if (res.status === 200) {
         setBio('')
         setDisplayName('')
