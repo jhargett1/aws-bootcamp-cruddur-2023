de2562be8d5dd7e5a6070201ad991f42932e3d2c
jhargett1 <joshhargett.jh@gmail.com>
2023-06-14 22:21:33 +0000

    Week-X Refactor Flask Routes - updated handle in seed.sql for seed data

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -9,7 +9,7 @@ VALUES
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'joshhargett' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'scubasteve' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
