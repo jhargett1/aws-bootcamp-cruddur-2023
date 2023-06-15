fbec87d85e97f454e5ed0a1a7966e32dfed1ed06
jhargett1 <joshhargett.jh@gmail.com>
2023-06-15 23:38:05 +0000

    Week-X Replies Work In Progress - added new migration file, added query to home.sql, altered query on object.sql, created reply.sql, refactored activities.py and create_reply.py, adjusted bin/db/migrate and bin/db/rollback, continuing to adjust and implement replies

--- frontend-react-js/src/components/ReplyForm.js
+++ frontend-react-js/src/components/ReplyForm.js
@@ -1,6 +1,7 @@
 import './ReplyForm.css';
 import React from "react";
 import process from 'process';
+import {getAccessToken} from '../lib/CheckAuth';
 
 import ActivityContent  from '../components/ActivityContent';
 
@@ -15,16 +16,21 @@ export default function ReplyForm(props) {
   }
 
   const onsubmit = async (event) => {
+    console.log('replyActivity', props.activity)
     event.preventDefault();
     try {
       const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/${props.activity.uuid}/reply`
+      await getAccessToken()
+      const access_token = localStorage.getItem("access_token")
       const res = await fetch(backend_url, {
         method: "POST",
         headers: {
+          'Authorization': `Bearer ${access_token}`,
           'Accept': 'application/json',
           'Content-Type': 'application/json'
         },
         body: JSON.stringify({
+          activity_uuid: props.activity.uuid, 
           message: message
         }),
       });
