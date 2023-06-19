31b1395f18060b9d849058452b1721923c6128b1
jhargett1 <joshhargett.jh@gmail.com>
2023-06-19 02:32:22 +0000

    Week X Cleanup Part 2 - created machineuser template.yaml

--- frontend-react-js/src/pages/MessageGroupPage.js
+++ frontend-react-js/src/pages/MessageGroupPage.js
@@ -20,15 +20,21 @@ export default function MessageGroupPage() {
 
   const loadMessageGroupsData = async () => {
     const url = `${process.env.REACT_APP_BACKEND_URL}/api/message_groups`
-    get(url,null,function(data){
-      setMessageGroups(data)
+    get(url,{
+      auth: true,
+      success: function(data){
+        setMessageGroups(data)
+      }
     })
   } 
 
   const loadMessageGroupData = async () => {
     const url = `${process.env.REACT_APP_BACKEND_URL}/api/messages/${params.message_group_uuid}`
-    get(url,null,function(data){
-      setMessages(data)
+    get(url,{
+      auth: true,
+      success: function(data){
+        setMessages(data)
+      }
     })
   }  
 
