fbf6870a16f61d8b636b3b1829148121e0681755
jhargett1 <joshhargett.jh@gmail.com>
2023-06-25 23:10:02 +0000

    Week X Additions - reply Cruds now expire - additional cleanup/adjustments on migrate and update_cognito_user_ids scripts

--- backend-flask/db/sql/activities/reply.sql
+++ backend-flask/db/sql/activities/reply.sql
@@ -1,7 +1,8 @@
 INSERT INTO public.activities (
   user_uuid,
   message,
-  reply_to_activity_uuid
+  reply_to_activity_uuid,
+  expires_at
 )
 VALUES (
   (SELECT uuid 
@@ -10,5 +11,6 @@ VALUES (
     LIMIT 1
   ),
   %(message)s,
-  %(reply_to_activity_uuid)s
+  %(reply_to_activity_uuid)s,
+  (SELECT expires_at FROM public.activities WHERE uuid = %(reply_to_activity_uuid)s)
 ) RETURNING uuid;
